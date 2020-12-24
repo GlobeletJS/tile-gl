e3b30aaacf241260dd36dd8b1c3369b3ffe63fb7
Jeshurun Hembd <jjhembd@gmail.com>
2020-12-24 14:38:30 -0600

    Fix bug in antimeridian check in preamble.glsl

--- src/preamble.glsl
+++ src/preamble.glsl
@@ -11,7 +11,9 @@ vec2 tileToMap(vec2 tilePos) {
   // Find distance of this tile from top left tile, in tile units
   float zoomFac = exp2(mapCoords.z - tileCoords.z);
   vec2 dTile = zoomFac * tileCoords.xy - mapCoords.xy;
-  dTile.x += (dTile.x < 0.0) ? exp2(mapCoords.z) : 0.0;
+  // tileCoords.x and mapCoords.x are both wrapped to the range [0..exp(z)]
+  // If the right edge of the tile is left of the map, we need to unwrap dTile
+  dTile.x += (dTile.x + zoomFac <= 0.0) ? exp2(mapCoords.z) : 0.0;
 
   // Convert to a translation in pixels
   vec2 tileTranslate = dTile * mapShift.z + mapShift.xy;
